6d45177acb5970c8201191b53109da396d38d169
Samuel de Medeiros Queiroz <samueldmq@gmail.com>
2021-04-01 20:19:51 -0300

    Add and test first demo

---
+++ artificial_intelligence/machine_learning/predict.py
@@ -0,0 +1,15 @@
+import numpy as np
+from tensorflow import keras
+
+# y = 5x + 2
+x = np.array([ -3.0, -2.0, -1.0, 0.0, 1.0,  2.0,  3.0], dtype=float)
+y = np.array([-13.0, -8.0, -3.0, 2.0, 7.0, 12.0, 17.0], dtype=float)
+
+model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+model.fit(x, y, epochs=500)
+
+output = model.predict([10.0])
+
+assert 52.001 < output[0][0] < 52.009
